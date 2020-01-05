96cf47e658a7a390546ecaf6711fbf5e17cad93c
dglushkov <Ltybc@2018>
2020-01-05 18:59:08 +0500

    DeliverReceipt - added done date and error

--- src/SMPP/Unit/DeliverReceiptSm.php
+++ src/SMPP/Unit/DeliverReceiptSm.php
@@ -3,12 +3,16 @@
 namespace PhpSmpp\SMPP\Unit;
 
 
+use PhpSmpp\Logger;
 use PhpSmpp\SMPP\Tag;
 
 class DeliverReceiptSm extends DeliverSm
 {
 
     public $state;
+    public $doneDate;
+    public $receiptErrorCode;
+    public $receiptErrorText;
 
     /** @inheritdoc */
     public function afterFill()
@@ -18,5 +22,29 @@ class DeliverReceiptSm extends DeliverSm
                 $this->state = (int)bin2hex($tag->value);
             }
         }
+        $this->parseDoneDate();
+        $this->parseError();
+    }
+
+    protected function parseDoneDate()
+    {
+        $numMatches = preg_match('/done date:(\d{10,12})/si', $this->shortMessage, $matches);
+        if ($numMatches == 0) {
+            Logger::debug('Could not parse delivery receipt: ' . $this->message . "\n" . bin2hex($this->body));
+        }
+        $dp = str_split($matches[1], 2);
+        $dd = gmmktime($dp[3], $dp[4], isset($dp[5]) ? $dp[5] : 0, $dp[1], $dp[2], $dp[0]);
+        $this->doneDate = new \DateTime("@$dd", new \DateTimeZone('UTC'));
+    }
+
+    protected function parseError()
+    {
+        $numMatches = preg_match('/edd:(\d+)/si', $this->shortMessage, $matches);
+        if ($numMatches == 0) {
+            Logger::debug('Could not parse error code: ' . $this->message . "\n" . bin2hex($this->body));
+        }
+        $this->receiptErrorCode = (int)$matches[1];
+        // TODO
+        $this->receiptErrorText = "Code:$this->receiptErrorCode";
     }
 }
