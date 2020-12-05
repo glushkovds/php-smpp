fafbe92a3617247315ff05f6a1d0b7f4e496d6bc
Subb98 <subb98@gmail.com>
2020-12-05 16:45:44 +0500

    Update ReadUSSDTest

--- src/Tests/ReadUSSDTest.php
+++ src/Tests/ReadUSSDTest.php
@@ -3,30 +3,32 @@
 namespace PhpSmpp\Tests;
 
 use PhpSmpp\Client;
-use PhpSmpp\Pdu\Ussd;
 use PhpSmpp\Pdu\Pdu;
+use PhpSmpp\Pdu\Ussd;
 use PhpSmpp\Service\Listener;
+use PhpSmpp\Service\Sender;
 use PhpSmpp\SMPP;
 use PhpSmpp\Transport\FakeTransport;
 use PHPUnit\Framework\TestCase;
 
 class ReadUSSDTest extends TestCase
 {
-    /**
-     * Checks the successful processing of an incoming USSD request.
-     */
     public function testReadUSSD(): void
     {
-        $service = new Listener([], '', '', Client::BIND_MODE_TRANSCEIVER);
-        $service->client->debug = true;
-        $service->client->setTransport(new FakeTransport());
+        $listener = new Listener([], '', '', Client::BIND_MODE_TRANSCEIVER);
+        $listener->client->debug = true;
+        $listener->client->setTransport(new FakeTransport());
+
+        $sender = new Sender([''], '', '', Client::BIND_MODE_TRANSCEIVER);
+        $sender->client->debug = true;
+        $sender->client->setTransport(new FakeTransport());
 
         /** @var FakeTransport $transport */
-        $transport = $service->client->getTransport();
+        $transport = $listener->client->getTransport();
         $transport->enqueueDeliverReceiptSm('deliver_sm_enquire_link');
         $transport->enqueueDeliverReceiptSm('deliver_sm_ussd');
 
-        $service->listenOnce(function (Pdu $pdu) {
+        $listener->listenOnce(function (Pdu $pdu) use ($sender) {
             /** @var Ussd $pdu */
             $this->assertInstanceOf(Ussd::class, $pdu);
             $this->assertEquals(SMPP::SERVICE_TYPE_USSD, $pdu->serviceType);
@@ -35,6 +37,9 @@ class ReadUSSDTest extends TestCase
             $this->assertEquals('*3322*0#', $pdu->message);
             $this->assertEquals(SMPP::DELIVER_SM, $pdu->id);
             $this->assertEquals(SMPP::ESME_ROK, $pdu->status);
+
+            $smsId = $sender->sendUSSD('992000000000', 'Your request has been accepted, wait for SMS confirmation', '3322', []);
+            $this->assertNotEmpty($smsId, 'Has no sms id');
         });
     }
 }
